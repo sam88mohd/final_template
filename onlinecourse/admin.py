62ba30e3c4a0fbb7b56f9fdfb3e2c9f5d43ea276
sam88mohd <sam88mohd@gmail.com>
2023-07-21 22:43:27 +0800

    implement question and choice features

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
 
 
 class LessonInline(admin.StackedInline):
     model = Lesson
+    extra = 5
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
     extra = 5
 
 
@@ -22,9 +27,14 @@ class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
 
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+
 # <HINT> Register Question and Choice models here
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
